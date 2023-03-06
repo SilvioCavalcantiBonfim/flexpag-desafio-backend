4ef649eacfc9162a1ed6f526174e6e1ba8a39d2f
silvio cavalcanti bonfim <silviocavalcanti2011@gmail.com>
2023-03-06 13:29:56 -0300

    final

---
+++ src/test/java/com/flexpag/paymentscheduler/service/PaymentServiceTest.java
@@ -0,0 +1,23 @@
+package com.flexpag.paymentscheduler.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PaymentServiceTest {
+
+    @Test
+    void createSchedule() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
